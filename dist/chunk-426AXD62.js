075cfd2f5b4a10356cfafff0f25c1ae28583639f
Pascal Thormeier <pascal.thormeier@liip.ch>
2024-09-24 10:39:03 +0200

    Add additional dist/ files for new version.

---
+++ dist/chunk-426AXD62.js
@@ -0,0 +1,11 @@
+import {
+  unpluginFactory
+} from "./chunk-AQBEMFH4.js";
+
+// src/vite.ts
+import { createVitePlugin } from "unplugin";
+var vite_default = createVitePlugin(unpluginFactory);
+
+export {
+  vite_default
+};
