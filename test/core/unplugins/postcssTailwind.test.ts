dd713d4ef3ad894edc5446d224b7640759479fa4
Pascal Thormeier <pascal.thormeier@liip.ch>
2024-12-17 15:29:17 +0100

    Fix up path mess in test.

--- test/core/unplugins/postcssTailwind.test.ts
+++ test/core/unplugins/postcssTailwind.test.ts
@@ -57,7 +57,7 @@ it('sets up the correct config for Vite', () => {
 
   expect(viteConfig).toStrictEqual({
     css: {
-      postcss: process.platform === 'win32' ? 'D:\\foo\\bar\\assets' : '/foo/bar/configs',
+      postcss: process.platform === 'win32' ? 'D:\\foo\\bar\\configs' : '/foo/bar/configs',
     },
   },
   )
@@ -299,7 +299,7 @@ it('sets up the correct config for rollup', async () => {
 
   expect(url).toHaveBeenCalledWith({
     assetsPath: 'dist',
-    basePath: process.platform === 'win32' ? 'D:\\foo\\bar\\assets' : '/foo/bar/configs',
+    basePath: process.platform === 'win32' ? 'D:\\foo\\bar\\assets' : '/foo/bar/assets',
     url: 'copy',
   })
 
